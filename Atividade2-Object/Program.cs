eeafa683ce1be75ddec79164209635c9a96aefdc
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-03-17 23:05:09 -0300

    feat: finished activity 2

---
+++ Atividade2-Object/Program.cs
@@ -0,0 +1,11 @@
+﻿object o = null;
+try
+{
+    Console.WriteLine(o.ToString());
+}
+catch (NullReferenceException)
+{
+    Console.WriteLine("O objeto é nulo, não é possivel chamar o metodo ToString()");
+    // Console.WriteLine("Tipo: " + e.GetType()); // System.NullReferenceException
+}
+
