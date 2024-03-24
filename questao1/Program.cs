2dd85acd0c733b65d4a6a70793cb6104bb779142
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-03-24 19:36:59 -0300

    Add new files and modify existing files

---
+++ questao1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+
+List<Double> lista = new List<double>{3, 7, 2, 4, 6};
+
+List<Double> novaLista = lista.ConvertAll( x => x/2);
+
+novaLista.ForEach(x => Console.WriteLine(x));
+
