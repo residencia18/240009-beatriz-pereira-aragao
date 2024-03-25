3019ddc1d0f219559b2c25e733350670784430d6
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-03-25 01:02:42 -0300

    feat: solução questao 3

---
+++ questao3/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using questao3.entities;
+
+Worker worker = new Worker();
+
+// Criando duas threads para executar o método DoWork
+Thread thread1 = new Thread(worker.DoWork);
+Thread thread2 = new Thread(worker.DoWork);
+
+// Iniciando as threads
+thread1.Start();
+thread2.Start();
+
+// Aguardando até que ambas as threads tenham terminado de executar
+thread1.Join();
+thread2.Join();
+
+Console.WriteLine("Ambas as threads terminaram. Programa encerrado.");
---
+++ questao3/entities/Worker.cs
@@ -0,0 +1,14 @@
+﻿namespace questao3.entities;
+
+class Worker
+{
+    public void DoWork()
+    {
+        for (int i = 1; i <= 2; i++)
+        {
+            Console.WriteLine($"Thread({Thread.CurrentThread.ManagedThreadId}): Tarefa {i} iniciada...");
+            Thread.Sleep(1000); // Simula algum processamento demorado
+            Console.WriteLine($"Thread({Thread.CurrentThread.ManagedThreadId}): Tarefa {i} concluída!");
+        }
+    }
+}
